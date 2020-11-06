aab3995245b1c7616a5c94507ac5ac935dea1faa
Daoud <99m.daoud@gmail.com>
2020-11-06 16:22:36 +0100

    Aggiunto esercizio + bonus

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// BONUS: Aggiungere un’immagine presa anch’essa da un data
 let app = new Vue({
     el: '#root',
     data: {
-
+        textTitle: 'Hello Boolean!!',
+        imageUrl: "#"
     },
     methods: {
-
+        showImage() {
+            if(this.imageUrl == "#") {
+                this.imageUrl = "https://memegenerator.net/img/instances/68132525.jpg";
+            } else {
+                this.imageUrl = "#";
+            }
+        }
     }
 });
